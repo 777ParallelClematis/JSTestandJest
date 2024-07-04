a8c11617d0df303e0819f88f754e8f0118da9851
Erin Colane <erin-colane@live.nmit.ac.nz>
2024-07-04 15:18:04 +1200

    added functions

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -8,13 +8,15 @@ function computerChoice(min, max){
    let minCeiled = Math.floor(0)
    let maxFloored = Math.ceil(3)
    let computerChoiceResult = Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled)
-}console.log(computerChoice())
+}console.log(computerChoiceResult)
 
 // get
 console.log(`${options[computerChoiceResult]}`)
 
 // function for user to choose rock, paper or scissors
-let userChoice = readlineSync.question
+let userChoice = readlineSync.question("Enter 'r', 'p' or 's': ")
+console.clear(`You entered ${userChoice}`)
+console.log(`The computer chose ${computerChoiceResult}`)
 
 
 // if theyre the same, return "Draw"
